d377c3bfdc723eee79b880a523104ed65223d8d8
daniel-liberto <daniel.liberto@hotmail.com>
2023-07-24 18:39:07 -0300

    creating and definiting login form

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,16 +5,16 @@ import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 const Header = () => {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to="/" aria-aria-label="Dogs - Home">
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
         <Link className={styles.login} to="/login">
           Login / Criar
         </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
