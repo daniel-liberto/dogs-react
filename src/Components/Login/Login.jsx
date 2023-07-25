522538e7946d41f1f709e7261a0654f09dd34426
daniel-liberto <daniel.liberto@hotmail.com>
2023-07-24 22:54:03 -0300

    starting form style

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -15,9 +15,9 @@ const Login = () => {
     <div>
       <Routes>
         <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
+        <Route path="create" element={<LoginCreate />} />
+        <Route path="passlost" element={<LoginPasswordLost />} />
+        <Route path="passreset" element={<LoginPasswordReset />} />
       </Routes>
     </div>
   );
